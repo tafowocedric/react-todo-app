e354a60a65987c419757a5a2f4e7c0041a4d224d
Vegas Cedric <vegascedric28@gmail.com>
2021-05-18 23:56:53 +0100

    todo application project folder structure

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class TodoContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Create React App</h1>
+                <p>I am in React component</p>
+            </div>
+        );
+    }
+}
+
+export default TodoContainer;
